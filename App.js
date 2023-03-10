a94cd792c69c6b63f1998725c588bd648183be39
LilAguh <aguezeochoa@gmail.com>
2023-03-10 19:49:48 -0300

    se prepara el entorno de desarrollo

--- App.js
+++ App.js
@@ -1,9 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
+import { LinearGradient } from 'expo-linear-gradient';
 import { StyleSheet, Text, View } from 'react-native';
+import { Path, Svg } from 'react-native-svg';
 
 export default function App() {
   return (
     <View style={styles.container}>
+      <LinearGradient colors={['#487F68', '#3A6353', '#3A6353', '#161817']} style={styles.gradient}>
+      </LinearGradient>
     </View>
   );
 }
@@ -11,8 +14,19 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'lightblue',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  gradient: {
+    width: "100%",
+    height: "100%",
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    color: "white",
+    fontSize: 82,
+    fontWeight: "bold"
+  }
 });
